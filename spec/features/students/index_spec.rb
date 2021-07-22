7817b419b4aa0d85e144ce69874012d8368e97b7
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-22 16:40:29 -0400

    Setup and user story 1 complete

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'The students index page' do
+  before(:each) do
+    @student1 = Student.create!(name: 'Harry Potter', age: 11, house: 'Gryffindor')
+    @student2 = Student.create!(name: 'Cedric Diggory', age: 14, house: 'Hufflepuff')
+    @student3 = Student.create!(name: 'Ron Weasley', age: 11, house: 'Gryffindor')
+    @student4 = Student.create!(name: 'Hermoine Granger', age: 11, house: 'Gryffindor')
+    @student5 = Student.create!(name: 'Draco Malfoy', age: 11, house: 'Slytherin')
+    @student6 = Student.create!(name: 'Pansy Parkinson', age: 11, house: 'Slytherin')
+    visit '/students'
+  end
+#   User Story 1 of 4
+#
+# As a visitor,
+# When I visit '/students',
+# I see a list of students with the following information:
+# -Name
+# -Age
+# -House
+# (e.g. "Name: Casseopia Black, Age: 14, House: Slytherin")
+  it 'lists all the students and their attributes' do
+    expect(page).to have_content(@student1.name)
+    expect(page).to have_content(@student4.age)
+    expect(page).to have_content(@student6.house)
+  end
+end
---
+++ spec/features/students/show_spec.rb
@@ -0,0 +1,9 @@
+# User Story 2 of 4
+#
+# As a visitor,
+# When I visit '/students/:id'
+# I see a list of the students' courses.
+#
+# (e.g. "Defense against the Dark Arts"
+#       "Herbology"
+#       "Potions")
